826c0385549f57c6d8f91e66dacb5f6fa5155f9a
MatheusAugustoBarbieri <barbiericonta@hotmail.com>
2022-06-21 00:09:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Comentarista name="Matheus" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Comentarista from "./components/Comentarista.vue";
+
+// componentes em TS
+@Component({
+  components: {
+    Comentarista,
+  },
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/Comentarista.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="comentar()">Comentar</button>
+    <p>
+      {{ comentario }}
+    </p>
+    <p>Nome: {{ name }}</p>
+  </div>
+</template>
+
+// lang = ts
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+// indica que essa class é um "COMPONENTE"
+@Component
+export default class Comentarista extends Vue {
+  // private da acesso somente dentro
+  // public da acesso ao template...etc
+
+  // data em TS
+  public comentario = "";
+  // prop em TS
+  // prop em TS
+  @Prop({ required: true, default: "Developer" }) public readonly name!: string;
+
+  // methods em TS
+  public comentar(): void {
+    this.comentario = "Um mega comentario bem bacana";
+  }
+  // computed em TS
+  get detalhes(): string {
+    return `Nome do comentarista: ${this.name}`;
+  }
+}
+</script>
+
+<style></style>
